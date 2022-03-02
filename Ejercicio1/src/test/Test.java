cc3f899eb90b23aae405b2339192e4019193ef85
AndresCuichan <andrescuichanflores@gmail.com>
2022-03-02 01:39:17 +0100

    Clase de Test modificado

--- Ejercicio1/src/test/Test.java
+++ Ejercicio1/src/test/Test.java
@@ -5,7 +5,7 @@ import domain.*;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("holaaa");
+		System.out.println("Suma");
 		Suma operacion1 = new Suma();
 		System.out.println(operacion1.SumaDosNumeros(12,6));
 		System.out.println(operacion1.SumaDosNumeros(2,8));
@@ -14,7 +14,15 @@ public class Test {
 		System.out.println(operacion1.SumaIncremento(4));
 		System.out.println(operacion1.SumaIncremento(4));
 		
-
+		System.out.println("Resta");
+		Resta operacion2 = new Resta();
+		System.out.println(operacion2.RestaDosNumeros(12,6));
+		System.out.println(operacion2.RestaDosNumeros(2,8));
+		
+		System.out.println(operacion2.numero);
+		System.out.println(operacion2.RestaIncremento(4));
+		System.out.println(operacion2.RestaIncremento(4));
+		
 	}
 
 }
